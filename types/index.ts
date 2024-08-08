92becd43860f98867cdec566fb6c8035f30208e1
Tane4ka170 <tane4ka170@gmail.com>
2024-08-08 23:28:37 +0300

    added SearchManufacterProps

--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,7 @@ export interface CustomButtonProps {
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
 }
+export interface SearchManufacterProps {
+  manufacturer: string;
+  setManuFacturer: (manufacturer: string) => void;
+}
